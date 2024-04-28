808ddbe49e407b4d232c8ff8cbd462f1a35195bd
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-04-28 12:22:27 +0300

    v-5-common/Created Button component, no logic

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+type TButtonProps = {
+    text: string,
+    handler: () => void,
+    bgColor?: 'purple' | 'transparent' | 'green',
+}
+
+const bgColorMap = {
+    'purple': 'bg-purple-300',
+    'transparent': 'bg-transparent',
+    'green': 'bg-green-300',
+}
+
+const Button = ({text, handler, bgColor = 'transparent'}: TButtonProps) => {
+
+    return (
+        <button className={`text-button text-black ${bgColorMap[bgColor] ? bgColorMap[bgColor] : ''} border border-black font-secondary px-8 py-2`} onClick={handler}>
+            {text}
+        </button>
+    );
+};
+
+export default Button;
