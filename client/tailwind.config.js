5e9e6e58b023d74413c5a1d153c8e9e7c2912a30
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-04-28 10:35:34 +0300

    v-1-common/Added custom font sizes to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'title-lg': '2.25rem', // 36px
+        'title-md': '1.875rem', // 30px
+        'title-sm': '1.5rem', // 24px
+
+        'label-lg': '1.25rem', // 20px
+        'label-md': '1rem', // 16px
+        'label-sm': '0.875rem', // 14px
+
+        'paragraph-lg': '1rem', // 16px
+        'paragraph-md': '0.875rem', // 14px
+        'paragraph-sm': '0.75rem', // 12px
+
+        'button': '1rem', // 16px
+      },
+    },
   },
   plugins: [],
 }
