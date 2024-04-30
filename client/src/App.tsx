918f55025a4d153232b59a50752a8228b54a4236
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-04-30 14:38:24 +0300

    created "Sign Up" modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import AnimRoutes from "./components/AnimRoutes";
-import {BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-    return (
-        <Router>
-            <div>Future navigation</div>
-            <AnimRoutes/>
-            <div>Future footer</div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>Future navigation</div>
+      <AnimRoutes />
+      <div>Future footer</div>
+    </Router>
+  );
 }
 
 export default App;
