49e69b1833d32c73df40b4e3d8a253b48a8d6ba5
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-05-01 22:41:10 +0300

    v-3-navigation/5/feat:Added Link to home on logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AnimRoutes from "./components/AnimRoutes";
-import { BrowserRouter as Router } from "react-router-dom";
+import {BrowserRouter as Router, Link} from "react-router-dom";
 import logo from './assets/logo.svg';
 import CustomLink from "./components/CustomLink";
 import { FaRobot } from "react-icons/fa";
@@ -15,7 +15,7 @@ function App() {
         <Router>
             <div className={'flex overflow-hidden h-screen w-screen flex-col'}>
                 <div className={'bg-[#339989] px-3 py-2 flex items-center'}>
-                    <img src={logo} alt="" className={'h-20'}/>
+                    <Link to={'/'}><img src={logo} alt="" className={'h-20'}/></Link>
                     <h1 className={'text-label-lg font-light font-secondary w-36 ml-3 border-r border-black'}>Cherkasy Print Racing Club</h1>
                     <div className={'flex gap-3 ml-auto px-10 w-fit'}>
                         <CustomLink link={'/'} text={'Single player'} icon={FaRobot}/>
