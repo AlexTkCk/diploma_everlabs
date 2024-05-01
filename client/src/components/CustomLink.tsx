0e19d370a525c8cfa95f4fb4d9cb2a2ddf2f4bd5
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-05-01 22:39:56 +0300

    v-3-navigation/4/fix:Fixed CustomLink overflow

--- client/src/components/CustomLink.tsx
+++ client/src/components/CustomLink.tsx
@@ -3,7 +3,7 @@ import { Link as RouterLink } from "react-router-dom";
 import { IconType } from "react-icons";
 
 interface CustomLinkProps {
-  icon?: IconType;
+  icon: IconType;
   text: string;
   link: string;
 }
@@ -12,12 +12,10 @@ const CustomLink: React.FC<CustomLinkProps> = ({ icon: Icon, text, link }) => {
   return (
     <RouterLink
       to={link}
-      className="relative flex flex-row gap-3 h-16 w-fit text-black ml-20 group"
+      className="flex flex-row items-center justify-center w-16 hover:w-fit duration-500 h-16 text-black group"
     >
-      {Icon && (
-        <Icon className="absolute h-16 w-16 top-1/2 left-1/2 p-2 transform -translate-x-1/2 -translate-y-1/2 bg-cyan  border-black border-2 rounded-full z-[1] group-hover:rotate-[360deg] group-hover:left-[-0.7rem] transition-all easy-in-out duration-500" />
-      )}
-      <p className="absolute font-primary text-xl w-fit text-nowrap top-1/2 left-1/2 opacity-0 transform  -translate-y-1/2 border-2 border-black p-2 pl-4  group-hover:translate-x-2 group-hover:opacity-100 transition-all duration-200 delay-200 easy-in-out">
+      <Icon className="h-16 shrink-0 w-16 top-1/2 left-1/2 p-2 bg-cyan  border-black border-2 rounded-full z-[1] group-hover:rotate-[360deg] group-hover:left-[-0.7rem] transition-all easy-in-out duration-500" />
+      <p className="font-primary text-xl border-0 opacity-0 text-nowrap border-black -translate-x-5 group-hover:-translate-x-2.5 group-hover:pl-4 group-hover:opacity-100 group-hover:w-full transition-all group-hover:border-2 group-hover:p-2 duration-500 overflow-hidden">
         {text}
       </p>
     </RouterLink>
