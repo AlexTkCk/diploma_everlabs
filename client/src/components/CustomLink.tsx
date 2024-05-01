524f8e43984adccc52133475f1d6884375a357ff
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-01 15:33:21 +0300

    v-3-navigation/1/fix:Fixed text z-index, nowrap

--- client/src/components/CustomLink.tsx
+++ client/src/components/CustomLink.tsx
@@ -17,7 +17,7 @@ const CustomLink: React.FC<CustomLinkProps> = ({ icon: Icon, text, link }) => {
       {Icon && (
         <Icon className="absolute h-16 w-16 top-1/2 left-1/2 p-2 transform -translate-x-1/2 -translate-y-1/2 bg-cyan  border-black border-2 rounded-full z-[1] group-hover:rotate-[360deg] group-hover:left-[-0.7rem] transition-all easy-in-out duration-500" />
       )}
-      <p className="absolute font-primary text-xl top-1/2 left-1/2 opacity-0 transform  -translate-y-1/2 border-2 border-black p-2 pl-4 z-[-1] group-hover:translate-x-2 group-hover:opacity-100 transition-all duration-200 delay-200 easy-in-out">
+      <p className="absolute font-primary text-xl w-fit text-nowrap top-1/2 left-1/2 opacity-0 transform  -translate-y-1/2 border-2 border-black p-2 pl-4  group-hover:translate-x-2 group-hover:opacity-100 transition-all duration-200 delay-200 easy-in-out">
         {text}
       </p>
     </RouterLink>
