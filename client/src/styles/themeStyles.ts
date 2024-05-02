21e8cf7028a9674fa75879dea0747d61317b6cf5
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-05-02 21:26:11 +0300

    v-12-theme/3/Created theme style colors map

---
+++ client/src/styles/themeStyles.ts
@@ -0,0 +1,27 @@
+export const themeStyles = {
+    green: {
+        primary: 'bg-green-950',
+        secondary: 'bg-green-400',
+        accent: 'bg-green-200',
+    },
+    yellow: {
+        primary: 'bg-yellow-950',
+        secondary: 'bg-yellow-400',
+        accent: 'bg-yellow-200',
+    },
+    red: {
+        primary: 'bg-red-950',
+        secondary: 'bg-red-400',
+        accent: 'bg-red-200',
+    },
+    blue: {
+        primary: 'bg-blue-950',
+        secondary: 'bg-blue-400',
+        accent: 'bg-blue-200',
+    },
+    black: {
+        primary: 'bg-black',
+        secondary: 'bg-black',
+        accent: 'bg-black',
+    }
+}
