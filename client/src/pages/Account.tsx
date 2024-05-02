451b70acff3003df8af5c6bde9acd9ce8f5bb489
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-05-02 12:37:33 +0300

    v-6-Account/1/created account page, removed const colors in component Button, added backgroundImage option in tailwind.config

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -1,17 +1,72 @@
-import React from 'react';
-import {pageVariants} from "../styles/variants";
-import {motion} from "framer-motion";
+import React from "react";
+import { pageVariants } from "../styles/variants";
+import { motion } from "framer-motion";
+import user_avatar from "../assets/user_avatar.jpeg";
+import Button from "../components/Button";
 
 const Account = () => {
-    return (
-        <motion.div className={''}
-                    variants={pageVariants}
-                    initial={'initial'}
-                    animate={'animate'}
-                    exit={'exit'}>
-            Account
-        </motion.div>
-    );
+  const data = {
+    name: "John Doe",
+    numberOfRaces: "10",
+    bestTime: "1:23:45",
+    placeInRanking: "1",
+    about:
+      "Integer malesuada libero a pellentesque viverra. Phasellus ac orci eget dolor tempor facilisis. Proin rhoncus dapibus diam a maximus. Phasellus nulla diam, commodo nec finibus sed, elementum vitae quam. Curabitur non blandit massa. Pellentesque ac tristique justo. Sed hendrerit nisl neque, ac fermentum risus tempus porta. Nulla tempus convallis ligula ac pellentesque. Nunc ut mi auctor, scelerisque quam vel, luctus lectus. Maecenas lacinia convallis lacus vel varius.",
+    startedPlaying: "02.05.2024",
+  };
+  return (
+    <motion.div
+      className={
+        "grow overflow-y-hidden flex flex-col items-center p-10 font-secondary"
+      }
+      variants={pageVariants}
+      initial={"initial"}
+      animate={"animate"}
+      exit={"exit"}
+    >
+      <div className="relative h-full w-3/4 flex flex-col gap-10 border-2 border-slate-900 p-10 rounded-md shadow-xl bg-account bg-contain bg-repeat">
+        <h2 className="text-5xl">Driver Licence</h2>
+        <div className="h-full flex flex-row gap-10">
+          <img
+            className="h-2/3 rounded-md border-2 border-slate-900"
+            src={user_avatar}
+            alt="user_avatar"
+          />
+          <div className="flex flex-col gap-5 font-primary text-xl">
+            <p>
+              <span className="font-bold">Name:</span>
+              {data.name}
+            </p>
+            <p>
+              {" "}
+              <span className="font-bold">Number of races:</span>{" "}
+              {data.numberOfRaces}
+            </p>
+            <p>
+              <span className="font-bold">Best time in the race:</span>{" "}
+              {data.bestTime}
+            </p>
+            <p>
+              <span className="font-bold">Place in the ranking table:</span>{" "}
+              {data.placeInRanking}
+            </p>
+            <p className="text-justify">
+              <span className="font-bold">About me:</span> {data.about}
+            </p>
+          </div>
+          <p className="absolute bottom-8 right-8 text-center text-xl">
+            Started playing: <br /> {data.startedPlaying}
+          </p>
+          <Button
+            buttonClassName="absolute top-8 right-8 bg-slate-200"
+            handler={() => {}}
+          >
+            Edit
+          </Button>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Account;
