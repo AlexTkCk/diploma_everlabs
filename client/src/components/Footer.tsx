5dc26cb8cf0bad09ee6f77d127409eeffafda94b
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-05-02 22:49:53 +0300

    v-12-theme/13/Changed Footer props, deleted unnecessary props

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,11 +6,11 @@ const Footer = () => {
         <div className={'flex justify-between bg-[#151E17] px-5 py-3 items-center'}>
             <p className={'font-secondary text-paragraph-lg text-white'}>© 2024 Pensil Games, Inc</p>
             <div className={'rounded-lg border border-white px-3 py-2 flex gap-3'}>
-                <ThemeButton bgColor={'green'} handler={() => {}} isActive={true}/>
-                <ThemeButton bgColor={'red'} handler={() => {}} isActive={false}/>
-                <ThemeButton bgColor={'yellow'} handler={() => {}} isActive={false}/>
-                <ThemeButton bgColor={'blue'} handler={() => {}} isActive={false}/>
-                <ThemeButton bgColor={'black'} handler={() => {}} isActive={false}/>
+                <ThemeButton bgColor={'green'} />
+                <ThemeButton bgColor={'red'} />
+                <ThemeButton bgColor={'yellow'} />
+                <ThemeButton bgColor={'blue'} />
+                <ThemeButton bgColor={'black'} />
             </div>
         </div>
     );
