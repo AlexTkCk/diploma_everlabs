fb5fa794e97b8f67008eb2cc410255430884124e
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-05-02 21:25:35 +0300

    v-12-theme/2/Defined theme context and theme config types

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,1 +1,14 @@
-export type TThemeColor = 'green' | 'red' | 'yellow' | 'black' | 'blue';
+import {Dispatch, SetStateAction} from "react";
+
+export type TThemeColor = 'green' | 'red' | 'blue' | 'yellow' | 'black';
+
+export type TThemeContext = {
+    themeConfig: TThemeConfig;
+    setTheme: Dispatch<SetStateAction<TThemeColor>>;
+}
+
+export type TThemeConfig = {
+    primary: string,
+    secondary: string,
+    accent: string,
+}
