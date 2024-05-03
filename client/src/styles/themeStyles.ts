c149a1c77487d60799837cc4ec03899d67e68ee6
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-05-03 23:34:55 +0300

    v-12-theme/23/Moved auto transition into themeStyle config, adjusted color pallets

--- client/src/styles/themeStyles.ts
+++ client/src/styles/themeStyles.ts
@@ -1,39 +1,43 @@
-const transitionStyle = '[transition:background-color_1s]';
-
 export const themeStyles = {
+    themeTransitionStyle: '[transition:background-color_1s]',
     green: {
-        primary: 'bg-[#011004] ' + transitionStyle,
-        secondary: 'bg-teal-700 ' + transitionStyle,
-        accent: 'bg-green-200 ' + transitionStyle,
+        primary: 'bg-[#011004] ',
+        secondary: 'bg-[#339989] ',
+        accent: 'bg-[#7DE2D1] ',
         hoverNeon: 'hover:shadow-neon-green border-2 border-white',
         neon: 'shadow-neon-green border-2 border-white',
+        info: 'bg-[#A3FCBC] ',
     },
     yellow: {
-        primary: 'bg-[#271D04] ' + transitionStyle,
-        secondary: 'bg-yellow-400 ' + transitionStyle,
-        accent: 'bg-yellow-200 ' + transitionStyle,
+        primary: 'bg-[#271D04] ',
+        secondary: 'bg-[#E8DA72] ',
+        accent: 'bg-[#FFF183] ',
         hoverNeon: 'hover:shadow-neon-yellow border-2 border-white',
         neon: 'shadow-neon-yellow border-2 border-white',
+        info: 'bg-[#FCE3A3] ',
     },
     red: {
-        primary: 'bg-[#260808] ' + transitionStyle,
-        secondary: 'bg-red-400 ' + transitionStyle,
-        accent: 'bg-red-200 ' + transitionStyle,
+        primary: 'bg-[#260808] ',
+        secondary: 'bg-[#E77272] ',
+        accent: 'bg-[#FFB9B9] ',
         hoverNeon: 'hover:shadow-neon-red border-2 border-white',
         neon: 'shadow-neon-red border-2 border-white',
+        info: 'bg-[#FCA3A3] ',
     },
     blue: {
-        primary: 'bg-[#02010B] ' + transitionStyle,
-        secondary: 'bg-blue-400 ' + transitionStyle,
-        accent: 'bg-blue-200 ' + transitionStyle,
+        primary: 'bg-[#02010B] ',
+        secondary: 'bg-[#72B1E8] ',
+        accent: 'bg-[#82D1FF] ',
         hoverNeon: 'hover:shadow-neon-blue border-2 border-white',
         neon: 'shadow-neon-blue border-2 border-white',
+        info: 'bg-[#A3ECFC] ',
     },
     black: {
-        primary: 'bg-black text-white ' + transitionStyle,
-        secondary: 'bg-black text-white ' + transitionStyle,
-        accent: 'bg-black text-white ' + transitionStyle,
+        primary: 'bg-black text-white ',
+        secondary: 'bg-black text-white ',
+        accent: 'bg-black text-white ',
         hoverNeon: 'border-2 border-white',
-        neon: 'border-2 border-white'
+        neon: 'border-2 border-white',
+        info: 'bg-black text-white ',
     }
 }
