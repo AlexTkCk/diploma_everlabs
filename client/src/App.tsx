46c168c70a0b8fc6929a33f6a213af9a3714340e
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-05-03 11:53:43 +0300

    v-7-Leaderboard/1/start to create the Leaderboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AnimRoutes from "./components/AnimRoutes";
-import {BrowserRouter as Router, Link} from "react-router-dom";
-import logo from './assets/logo.svg';
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import logo from "./assets/logo.svg";
 import CustomLink from "./components/CustomLink";
 import { FaRobot } from "react-icons/fa";
 import { FaUsers } from "react-icons/fa";
@@ -9,36 +9,64 @@ import { MdLeaderboard } from "react-icons/md";
 import { BiSolidUserAccount } from "react-icons/bi";
 import ThemeButton from "./components/ThemeButton";
 
-
-
 function App() {
-    return (
-        <Router>
-            <div className={'flex overflow-hidden h-screen w-screen flex-col'}>
-                <div className={'bg-[#339989] px-3 py-2 flex items-center'}>
-                    <Link to={'/'}><img src={logo} alt="" className={'h-20'}/></Link>
-                    <h1 className={'text-label-lg font-light font-secondary w-36 ml-3 border-r border-black'}>Cherkasy Print Racing Club</h1>
-                    <div className={'flex gap-3 ml-auto px-10 w-fit'}>
-                        <CustomLink link={'/'} text={'Single player'} icon={FaRobot}/>
-                        <CustomLink link={'/'} text={'Multiplayer'} icon={FaUsers}/>
-                        <CustomLink link={'/'} text={'Leaderboard'} icon={MdLeaderboard}/>
-                        <CustomLink link={'/account'} text={'Account'} icon={BiSolidUserAccount}/>
-                    </div>
-                </div>
-                <AnimRoutes/>
-                <div className={'flex justify-between bg-[#151E17] px-5 py-3 items-center'}>
-                    <p className={'font-secondary text-paragraph-lg text-white'}>© 2024 Pensil Games, Inc</p>
-                    <div className={'rounded-lg border border-white px-3 py-2 flex gap-3'}>
-                        <ThemeButton bgColor={'green'} handler={() => {}} isActive={true}/>
-                        <ThemeButton bgColor={'red'} handler={() => {}} isActive={false}/>
-                        <ThemeButton bgColor={'yellow'} handler={() => {}} isActive={false}/>
-                        <ThemeButton bgColor={'blue'} handler={() => {}} isActive={false}/>
-                        <ThemeButton bgColor={'black'} handler={() => {}} isActive={false}/>
-                    </div>
-                </div>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className={"flex overflow-hidden h-screen w-screen flex-col"}>
+        <div className={"bg-[#339989] px-3 py-2 flex items-center"}>
+          <Link to={"/"}>
+            <img src={logo} alt="" className={"h-20"} />
+          </Link>
+          <h1
+            className={
+              "text-label-lg font-light font-secondary w-36 ml-3 border-r border-black"
+            }
+          >
+            Cherkasy Print Racing Club
+          </h1>
+          <div className={"flex gap-3 ml-auto px-10 w-fit"}>
+            <CustomLink link={"/"} text={"Single player"} icon={FaRobot} />
+            <CustomLink link={"/"} text={"Multiplayer"} icon={FaUsers} />
+            <CustomLink
+              link={"/leaderboard"}
+              text={"Leaderboard"}
+              icon={MdLeaderboard}
+            />
+            <CustomLink
+              link={"/account"}
+              text={"Account"}
+              icon={BiSolidUserAccount}
+            />
+          </div>
+        </div>
+        <AnimRoutes />
+        <div
+          className={"flex justify-between bg-[#151E17] px-5 py-3 items-center"}
+        >
+          <p className={"font-secondary text-paragraph-lg text-white"}>
+            © 2024 Pensil Games, Inc
+          </p>
+          <div
+            className={"rounded-lg border border-white px-3 py-2 flex gap-3"}
+          >
+            <ThemeButton bgColor={"green"} handler={() => {}} isActive={true} />
+            <ThemeButton bgColor={"red"} handler={() => {}} isActive={false} />
+            <ThemeButton
+              bgColor={"yellow"}
+              handler={() => {}}
+              isActive={false}
+            />
+            <ThemeButton bgColor={"blue"} handler={() => {}} isActive={false} />
+            <ThemeButton
+              bgColor={"black"}
+              handler={() => {}}
+              isActive={false}
+            />
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/pages/LeaderBoard.tsx
+++ client/src/pages/LeaderBoard.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
-import {pageVariants} from "../styles/variants";
-import {motion} from "framer-motion";
+import React from "react";
+import { pageVariants } from "../styles/variants";
+import { motion } from "framer-motion";
 
 const LeaderBoard = () => {
-    return (
-        <motion.div className={''}
-                    variants={pageVariants}
-                    initial={'initial'}
-                    animate={'animate'}
-                    exit={'exit'}>
-            LeaderBoard
-        </motion.div>
-    );
+  return (
+    <motion.div
+      className={"grow overflow-y-hidden flex flex-col"}
+      variants={pageVariants}
+      initial={"initial"}
+      animate={"animate"}
+      exit={"exit"}
+    >
+      <div className="w-full h-full flex flex-col gap-5 px-10 py-2 items-center font-primary">
+        <input
+          placeholder="Find a player"
+          type="search"
+          className="w-1/4 h-12 p-4 border-2 border-black rounded-2xl"
+        />
+        <div className="flex flex-row gap-10 w-full h-auto grow font-secondary">
+          <div className="w-3/4 h-full border-2 border-black rounded-2xl py-5 px-2">
+            <div className="flex flex-row justify-around pb-2 text-xl font-bold">
+              <span className="w-1/3 text-center ">Name</span>
+              <span className="w-1/3 text-center">Speed</span>
+              <span className="w-1/3 text-center">Time</span>
+            </div>
+            <hr className="border-black" />
+          </div>
+
+          <div className="w-1/4 h-full border-2 border-black rounded-2xl py-5 px-2 font-primary flex flex-col gap-3 items-center">
+            <span className="text-center text-3xl">Filter</span>
+            <hr className="border-black w-full" />
+            <div className="flex gap-5 text-2xl">
+              <label htmlFor="rt">Race type</label>
+              <input type="checkbox" id="rt" />
+            </div>
+            <div className="flex gap-5 text-2xl">
+              <label htmlFor="ms">My scores</label>
+              <input type="checkbox" id="ms" />
+            </div>
+            <div className="flex gap-5 text-2xl">
+              <label htmlFor="comp">Competitions</label>
+              <input type="checkbox" id="comp" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default LeaderBoard;
