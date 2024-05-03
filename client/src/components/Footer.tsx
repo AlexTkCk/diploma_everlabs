4d2d191bb44746ef49b5d1c4c4a47207fcb6359e
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-05-03 23:37:02 +0300

    v-12-theme/27/Added bg-color transition to Footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,13 +1,14 @@
 import React, {useContext} from 'react';
 import ThemeButton from "./ThemeButton";
 import {themeContext} from "../context/ThemeContext";
+import {themeStyles} from "../styles/themeStyles";
 
 const Footer = () => {
 
     const {themeConfig} = useContext(themeContext);
 
     return (
-        <div className={`flex justify-between ${themeConfig.primary} px-5 py-3 items-center z-10`}>
+        <div className={`flex justify-between ${themeConfig.primary} ${themeStyles.themeTransitionStyle} px-5 py-3 items-center`}>
             <p className={'font-secondary text-paragraph-lg text-white'}>© 2024 Pensil Games, Inc</p>
             <div className={'rounded-lg border border-white px-3 py-2 flex gap-3'}>
                 <ThemeButton bgColor={'green'} />
