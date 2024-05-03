f9cc69137fa8d34ea9a89b3e678d37eef8a4ce95
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-03 21:50:45 +0300

    v-12-theme/20/Added custom colors to the theme, added transition

--- client/src/styles/themeStyles.ts
+++ client/src/styles/themeStyles.ts
@@ -1,38 +1,38 @@
-const transitionStyle = ' transition-colors duration-300 ease-in-out';
+const transitionStyle = '[transition:background-color_1s]';
 
 export const themeStyles = {
     green: {
-        primary: 'bg-green-950 ' + transitionStyle,
+        primary: 'bg-[#011004] ' + transitionStyle,
         secondary: 'bg-teal-700 ' + transitionStyle,
         accent: 'bg-green-200 ' + transitionStyle,
         hoverNeon: 'hover:shadow-neon-green border-2 border-white',
         neon: 'shadow-neon-green border-2 border-white',
     },
     yellow: {
-        primary: 'bg-yellow-950 ' + transitionStyle,
+        primary: 'bg-[#271D04] ' + transitionStyle,
         secondary: 'bg-yellow-400 ' + transitionStyle,
         accent: 'bg-yellow-200 ' + transitionStyle,
         hoverNeon: 'hover:shadow-neon-yellow border-2 border-white',
         neon: 'shadow-neon-yellow border-2 border-white',
     },
     red: {
-        primary: 'bg-red-950 ' + transitionStyle,
+        primary: 'bg-[#260808] ' + transitionStyle,
         secondary: 'bg-red-400 ' + transitionStyle,
         accent: 'bg-red-200 ' + transitionStyle,
         hoverNeon: 'hover:shadow-neon-red border-2 border-white',
         neon: 'shadow-neon-red border-2 border-white',
     },
     blue: {
-        primary: 'bg-blue-950 ' + transitionStyle,
+        primary: 'bg-[#02010B] ' + transitionStyle,
         secondary: 'bg-blue-400 ' + transitionStyle,
         accent: 'bg-blue-200 ' + transitionStyle,
         hoverNeon: 'hover:shadow-neon-blue border-2 border-white',
         neon: 'shadow-neon-blue border-2 border-white',
     },
     black: {
-        primary: 'bg-black transition-colors duration-250',
-        secondary: 'bg-black transition-colors duration-250',
-        accent: 'bg-black transition-colors duration-250',
+        primary: 'bg-black text-white ' + transitionStyle,
+        secondary: 'bg-black text-white ' + transitionStyle,
+        accent: 'bg-black text-white ' + transitionStyle,
         hoverNeon: 'border-2 border-white',
         neon: 'border-2 border-white'
     }
