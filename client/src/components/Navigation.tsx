04399160a2392f4445fa5d89866a8917dd937f1c
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-05-03 23:38:44 +0300

    v-12-theme/30/Added bg-color theme transition to Navigation

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -6,12 +6,13 @@ import {FaRobot, FaUsers} from "react-icons/fa";
 import {MdLeaderboard} from "react-icons/md";
 import {BiSolidUserAccount} from "react-icons/bi";
 import {themeContext} from "../context/ThemeContext";
+import {themeStyles} from "../styles/themeStyles";
 
 const Navigation = () => {
     const {themeConfig} = useContext(themeContext);
 
     return (
-        <div className={`${themeConfig.secondary} px-3 py-2 flex items-center`}>
+        <div className={`${themeConfig.secondary} ${themeStyles.themeTransitionStyle} px-3 py-2 flex items-center`}>
             <Link to={'/'}><img src={logo} alt="" className={'h-20'}/></Link>
             <h1 className={'text-label-lg font-light font-secondary w-36 ml-3 border-r border-black'}>Cherkasy Print Racing Club</h1>
             <div className={'flex gap-3 ml-auto px-10 w-fit'}>
