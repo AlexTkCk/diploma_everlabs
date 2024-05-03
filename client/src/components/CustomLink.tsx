3a5e1fe89831133c20c56f1f918fe233d46a29bc
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-05-03 23:37:32 +0300

    v-12-theme/28/Added transition to CustomLink

--- client/src/components/CustomLink.tsx
+++ client/src/components/CustomLink.tsx
@@ -20,7 +20,7 @@ const CustomLink: React.FC<CustomLinkProps> = ({ icon: Icon, text, link }) => {
       <p className={`font-primary text-xl border-0 opacity-0 text-nowrap border-black -translate-x-5 group-hover:-translate-x-2.5 group-hover:pl-4 group-hover:opacity-100 group-hover:w-full transition-all group-hover:border-2 group-hover:p-2 duration-500 overflow-hidden ${themeConfig.accent}`}>
         {text}
       </p>
-      <Icon className={`h-16 shrink-0 w-16 top-1/2 left-1/2 p-2 ${themeConfig.accent} border-black border-2 rounded-full group-hover:rotate-[360deg] group-hover:left-[-0.7rem] transition-transform easy-in-out duration-500`} />
+      <Icon className={`h-16 shrink-0 w-16 top-1/2 left-1/2 p-2 ${themeConfig.accent} border-black border-2 rounded-full group-hover:rotate-[360deg] group-hover:left-[-0.7rem] transition duration-500`} />
     </RouterLink>
   );
 };
