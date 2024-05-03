097663b7d7bedd4ae9bbd939bc8378c6ba060593
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-05-03 14:29:41 +0300

    v-6-Account/2/trying to fix bug with text in "About"

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -11,24 +11,24 @@ const Account = () => {
     bestTime: "1:23:45",
     placeInRanking: "1",
     about:
-      "Integer malesuada libero a pellentesque viverra. Phasellus ac orci eget dolor tempor facilisis. Proin rhoncus dapibus diam a maximus. Phasellus nulla diam, commodo nec finibus sed, elementum vitae quam. Curabitur non blandit massa. Pellentesque ac tristique justo. Sed hendrerit nisl neque, ac fermentum risus tempus porta. Nulla tempus convallis ligula ac pellentesque. Nunc ut mi auctor, scelerisque quam vel, luctus lectus. Maecenas lacinia convallis lacus vel varius.",
+      "Integer malesuada libero a pellentesque viverra. Phasellus ac orci eget dolor tempor facilisis. Proin rhoncus dapibus diam a maximus. Phasellus nulla diam, commodo nec finibus sed, elementum vitae quam. Curabitur non blandit massa. Pellentesque ac tristique justo.",
     startedPlaying: "02.05.2024",
   };
   return (
     <motion.div
       className={
-        "grow overflow-y-hidden flex flex-col items-center p-10 font-secondary"
+        "grow overflow-y-hidden flex flex-col items-center p-5 font-secondary"
       }
       variants={pageVariants}
       initial={"initial"}
       animate={"animate"}
       exit={"exit"}
     >
-      <div className="relative h-full w-3/4 flex flex-col gap-10 border-2 border-slate-900 p-10 rounded-md shadow-xl bg-account bg-contain bg-repeat">
+      <div className="relative h-[95%] w-3/4 flex flex-col gap-10 border-2 border-slate-900 p-10 pb-24 rounded-md shadow-xl bg-account bg-contain bg-repeat">
         <h2 className="text-5xl">Driver Licence</h2>
         <div className="h-full flex flex-row gap-10">
           <img
-            className="h-2/3 rounded-md border-2 border-slate-900"
+            className="h-[80%] rounded-md border-2 border-slate-900"
             src={user_avatar}
             alt="user_avatar"
           />
@@ -50,15 +50,15 @@ const Account = () => {
               <span className="font-bold">Place in the ranking table:</span>{" "}
               {data.placeInRanking}
             </p>
-            <p className="text-justify">
+            <p className="text-justify w-3/4 overflow-y-scroll">
               <span className="font-bold">About me:</span> {data.about}
             </p>
           </div>
-          <p className="absolute bottom-8 right-8 text-center text-xl">
+          <p className="absolute bottom-3 right-3 text-center text-md font-bold">
             Started playing: <br /> {data.startedPlaying}
           </p>
           <Button
-            buttonClassName="absolute top-8 right-8 bg-slate-200"
+            buttonClassName="absolute top-5 right-5 bg-slate-200"
             handler={() => {}}
           >
             Edit
