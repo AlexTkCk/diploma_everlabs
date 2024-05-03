b5e59306a9383d0cac3b47f3563738613fb9ba57
Vidmenko Oleksij <avidmemko3@gmail.com>
2024-05-03 13:17:46 +0300

    server/2/add jwt token, testing in some_controller,emty application  ,stable version

--- server/myapp/config/routes.rb
+++ server/myapp/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   post '/user/signup', to: 'users#signup'
   post '/user/login', to: 'users#login'
+  get '/user/logout', to: 'users#logout'
+  post '/some_action', to: 'some#some_action'
 end
