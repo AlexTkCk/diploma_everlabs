e9c9fc5caac17550c6566bb05794883dba0ca337
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-03 21:49:55 +0300

    v-12-theme/19/Added theme config to the CustomLink.tsx

--- client/src/components/CustomLink.tsx
+++ client/src/components/CustomLink.tsx
@@ -17,10 +17,10 @@ const CustomLink: React.FC<CustomLinkProps> = ({ icon: Icon, text, link }) => {
       to={link}
       className="flex flex-row-reverse items-center justify-center w-16 hover:w-fit duration-500 h-16 text-black group"
     >
-      <p className="font-primary text-xl border-0 opacity-0 text-nowrap border-black -translate-x-5 group-hover:-translate-x-2.5 group-hover:pl-4 group-hover:opacity-100 group-hover:w-full transition-all group-hover:border-2 group-hover:p-2 duration-500 overflow-hidden">
+      <p className={`font-primary text-xl border-0 opacity-0 text-nowrap border-black -translate-x-5 group-hover:-translate-x-2.5 group-hover:pl-4 group-hover:opacity-100 group-hover:w-full transition-all group-hover:border-2 group-hover:p-2 duration-500 overflow-hidden ${themeConfig.accent}`}>
         {text}
       </p>
-      <Icon className={`h-16 shrink-0 w-16 top-1/2 left-1/2 p-2 ${themeConfig.accent} border-black border-2 rounded-full group-hover:rotate-[360deg] group-hover:left-[-0.7rem] transition-all easy-in-out duration-500`} />
+      <Icon className={`h-16 shrink-0 w-16 top-1/2 left-1/2 p-2 ${themeConfig.accent} border-black border-2 rounded-full group-hover:rotate-[360deg] group-hover:left-[-0.7rem] transition-transform easy-in-out duration-500`} />
     </RouterLink>
   );
 };
