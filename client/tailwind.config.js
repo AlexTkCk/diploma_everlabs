19405311ca8196892afdc26019611c97a6614b49
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-08 17:30:31 +0300

    v-9-Race-room/4/Changed inner neon shadow style

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         'neon-blue': '0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #00f, 0 0 15px #00f, 0 0 30px #00f',
         'neon-yellow': '0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #ff0, 0 0 15px #ff0, 0 0 30px #ff0',
         'neon-inner':
-          "inset 0 0 10px #00ff00, inset 0 0 20px #00ff00, inset 0 0 30px #00ff00, inset 0 0 40px #00ff00, inset 0 0 70px #00ff00, inset 0 0 80px #00ff00, inset 0 0 100px #00ff00, inset 0 0 150px #00ff00",
+          "inset 0 0 10px #00ff00, inset 0 0 20px #00ff00",
 
         buttonHover: "inset 6.5em 0 0 0",
       },
