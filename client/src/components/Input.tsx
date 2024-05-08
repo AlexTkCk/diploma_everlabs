bbc3ca0dbb910bda103ce99e923104a2a683ebc6
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-05-08 10:31:06 +0300

    L-6-Account/added editModal as component, u can rewrite name, "about me" and pic of user, after reload of page info will be return to start state, will fixed after adding the API

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React from "react";
 
 type TInput = {
-    placeholder? : string;
-    labelText: string;
-    changeHandler: () => void,
-    type?: 'text' | 'password'
-}
+  placeholder?: string;
+  labelText: string;
+  changeHandler: (e: any) => void;
+  type?: "text" | "password";
+};
 
-const Input = ({placeholder='', labelText, changeHandler, type='text'}: TInput) => {
-    return (
-        <label className={'flex flex-col font-primary text-title-lg'}>
-            {labelText}
-            <input onChange={changeHandler} type={type} placeholder={placeholder} className={'font-secondary text-label-lg rounded-full px-3'}/>
-        </label>
-    );
+const Input = ({
+  placeholder = "",
+  labelText,
+  changeHandler,
+  type = "text",
+}: TInput) => {
+  return (
+    <label className={"flex flex-col font-primary text-title-lg"}>
+      {labelText}
+      <input
+        onChange={changeHandler}
+        type={type}
+        placeholder={placeholder}
+        className={"font-secondary text-label-lg rounded-full px-3"}
+      />
+    </label>
+  );
 };
 
 export default Input;
