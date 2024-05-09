5285198c3e8684cc200195c949b00404b1f332ce
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-05-09 11:58:07 +0300

    v-7-Leaderboard/5/resized font in RowLb

--- client/src/components/RowLb.tsx
+++ client/src/components/RowLb.tsx
@@ -11,7 +11,7 @@ type TRowLb = {
 
 const RowLb: React.FC<TRowLb> = ({ dataLB }) => {
   return (
-    <div className="flex flex-row justify-around text-center">
+    <div className="flex flex-row justify-around text-center text-xl">
       <span className="w-1/3">{dataLB.name}</span>
       <span className="w-1/3">{dataLB.speed}</span>
       <span className="w-1/3">{dataLB.time}</span>
