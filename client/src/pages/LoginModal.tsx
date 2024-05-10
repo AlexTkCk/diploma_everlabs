3d633296789c942f39888cc4e78a9e8a06250a19
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-10 17:09:01 +0300

    v-view-authentication/7/Moved authentication modals as a pages

--- client/src/pages/LoginModal.tsx
+++ client/src/pages/LoginModal.tsx
@@ -1,21 +1,22 @@
 import React, {useContext} from "react";
-import Input from "./Input";
-import Button from "./Button";
+import Input from "../components/Input";
+import Button from "../components/Button";
 import { FcGoogle } from "react-icons/fc";
 import {themeContext} from "../context/ThemeContext";
+import {Link} from "react-router-dom";
 
-const SignUpModal = () => {
+const LoginModal = () => {
 
     const {themeConfig} = useContext(themeContext);
 
   return (
-    <div className={"fixed top-0 left-0 w-full h-full grid place-items-center"}>
+    <div className={"relative w-full h-full grid place-items-center"}>
       <div
         className={"absolute top-0 left-0 w-full h-full bg-black opacity-80"}
       ></div>
       <div
         className={
-          `w-1/3 -skew-x-12 ${themeConfig.neon} ${themeConfig.info} py-5 px-5`
+          `w-fit -skew-x-12 ${themeConfig.neon} ${themeConfig.info} py-5 border-2 px-5`
         }
       >
         <div className={"mx-auto flex flex-col gap-10"}>
@@ -29,19 +30,16 @@ const SignUpModal = () => {
             changeHandler={() => {}}
             labelText={"Password"}
           />
-          <Input
-            placeholder={"*********"}
-            changeHandler={() => {}}
-            labelText={"Confirm pass"}
-          />
-          <div className={"flex justify-center items-center gap-5"}>
+          <div className={"flex justify-between items-center gap-2"}>
             <Button
               handler={() => {}}
+
               buttonClassName="hover:shadow-buttonHover hover:shadow-blue-500 transition-all duration-500 hover:text-white bg-purple-300"
             >
-                Sign up
+                Log in
             </Button>
             <FcGoogle className={"text-7xl stroke-black stroke-1"} />
+            <Link to={'/signUp'} className={"text-title-md text-nowrap"}>... or Sign Up</Link>
           </div>
         </div>
       </div>
@@ -49,4 +47,4 @@ const SignUpModal = () => {
   );
 };
 
-export default SignUpModal;
+export default LoginModal;
--- client/src/pages/SignUpModal.tsx
+++ client/src/pages/SignUpModal.tsx
@@ -1,21 +1,21 @@
 import React, {useContext} from "react";
-import Input from "./Input";
-import Button from "./Button";
+import Input from "../components/Input";
+import Button from "../components/Button";
 import { FcGoogle } from "react-icons/fc";
 import {themeContext} from "../context/ThemeContext";
 
-const LoginModal = () => {
+const SignUpModal = () => {
 
     const {themeConfig} = useContext(themeContext);
 
   return (
-    <div className={"fixed top-0 left-0 w-full h-full grid place-items-center"}>
-      <div
-        className={"absolute top-0 left-0 w-full h-full bg-black opacity-80"}
-      ></div>
+      <div className={"relative w-full h-full grid place-items-center"}>
+          <div
+              className={"absolute top-0 left-0 w-full h-full bg-black opacity-80"}
+          ></div>
       <div
         className={
-          `w-fit -skew-x-12 ${themeConfig.neon} ${themeConfig.info} py-5 border-2 px-5`
+          `w-1/3 -skew-x-12 ${themeConfig.neon} ${themeConfig.info} py-5 px-5`
         }
       >
         <div className={"mx-auto flex flex-col gap-10"}>
@@ -29,16 +29,19 @@ const LoginModal = () => {
             changeHandler={() => {}}
             labelText={"Password"}
           />
-          <div className={"flex justify-between items-center gap-2"}>
+          <Input
+            placeholder={"*********"}
+            changeHandler={() => {}}
+            labelText={"Confirm pass"}
+          />
+          <div className={"flex justify-center items-center gap-5"}>
             <Button
               handler={() => {}}
-
               buttonClassName="hover:shadow-buttonHover hover:shadow-blue-500 transition-all duration-500 hover:text-white bg-purple-300"
             >
-                Log in
+                Sign up
             </Button>
             <FcGoogle className={"text-7xl stroke-black stroke-1"} />
-            <span className={"text-title-md text-nowrap"}>... or Sign Up</span>
           </div>
         </div>
       </div>
@@ -46,4 +49,4 @@ const LoginModal = () => {
   );
 };
 
-export default LoginModal;
+export default SignUpModal;
