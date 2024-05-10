00c281490ad48d2b639bb3f6d278fdfd6217e073
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-10 15:49:02 +0300

    v-view-authentication/3/Wrapped app into UserContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,19 @@ import {BrowserRouter as Router} from "react-router-dom";
 import ThemeProvider from "./context/ThemeContext";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
+import UserContext from "./context/UserContext";
 
 function App() {
     return (
         <Router>
             <ThemeProvider>
-                <div className={'flex overflow-hidden h-screen w-screen flex-col'}>
-                    <Navigation/>
-                    <AnimRoutes/>
-                    <Footer/>
-                </div>
+                <UserContext>
+                    <div className={'flex overflow-hidden h-screen w-screen flex-col'}>
+                        <Navigation/>
+                        <AnimRoutes/>
+                        <Footer/>
+                    </div>
+                </UserContext>
             </ThemeProvider>
         </Router>
     );
