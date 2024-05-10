fa4ce217bc3f3c06492c11090a6824498dca1b6d
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-10 14:17:45 +0300

    v-9-Race-room/20/Added title and leave the room button, adjusted modal size

--- client/src/components/SessionLineChar.tsx
+++ client/src/components/SessionLineChar.tsx
@@ -8,6 +8,8 @@ import {
     ResponsiveContainer,
 } from 'recharts';
 import {themeContext} from "../context/ThemeContext";
+import Button from "./Button";
+import {useNavigate} from "react-router";
 
 export interface SessionData {
     sps: number;
@@ -21,15 +23,16 @@ interface SessionLineChartProps {
 
 const SessionLineChart = ({ sessionData } : SessionLineChartProps) => {
 
+    const navigate = useNavigate();
     const {themeConfig} = useContext(themeContext);
 
     return (
-        <div className={'absolute w-full h-full grid place-items-center'}>
-            <div className={'absolute w-full h-full opacity-80 bg-black'}>
+        <div className={'absolute w-full h-full flex flex-col pt-20 items-center'}>
+            <div className={'absolute top-0 left-0 w-full h-full opacity-90 bg-black'}>
 
             </div>
-
-            <ResponsiveContainer width={'30%'} height={'30%'} className={`px-2 z-10 py-5 border ${themeConfig.primary} border-white border-2 rounded-xl ${themeConfig.neon}`}>
+            <h1 className={`text-white text-8xl ${themeConfig.primary} rounded-xl px-5 py-5 my-12 shadow-white shadow-lg font-secondary z-10`}>Results</h1>
+            <ResponsiveContainer width={'30%'} height={'30%'} className={`z-10 px-5 py-5 border ${themeConfig.primary}  border-white border-2 rounded-xl ${themeConfig.neon}`}>
                 <LineChart data={sessionData}>
                     <XAxis dataKey={'name'}/>
                     <CartesianGrid />
@@ -39,6 +42,7 @@ const SessionLineChart = ({ sessionData } : SessionLineChartProps) => {
 
                 </LineChart>
             </ResponsiveContainer>
+            <Button buttonClassName={`z-10 ${themeConfig.accent} mt-5`} handler={() => (navigate('/'))}>Leave the race</Button>
         </div>
     );
 };
