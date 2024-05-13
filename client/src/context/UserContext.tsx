55a0658a404bbe1e15861c9c1e5f5f806cf467d8
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-13 14:01:36 +0300

    v-view-authentication/17/Separated user onto userId and userId

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,25 +1,33 @@
-import React, {createContext, Dispatch, SetStateAction, useEffect, useState} from 'react';
-import {TUser} from "../types";
+import React, {createContext, Dispatch, SetStateAction, useState} from 'react';
+import {TUserData} from "../types";
+import {v4 as uuid4} from 'uuid';
 
 type TUserContextProps = {
     children: React.ReactNode;
 }
 
 type TUserContext = {
-    user: TUser,
-    setUser: Dispatch<SetStateAction<TUser>>
+    userId: string,
+    userData: TUserData,
+    setUserId: Dispatch<SetStateAction<string>>,
+    setUserData: Dispatch<SetStateAction<TUserData>>,
 }
 
 export const userContext = createContext<TUserContext>({
-    user: null,
-    setUser: () => {}
+    userId: '',
+    userData: null,
+    setUserId: () => {},
+    setUserData: () => {}
 });
 
 const UserContext = ({children}: TUserContextProps) => {
-    const [user, setUser] = useState<TUser>(null);
+    const [userId, setUserId] = useState(uuid4());
+    const [userData, setUserData] = useState<TUserData>(null);
+
+    console.log(userId)
 
     return (
-        <userContext.Provider value={{user, setUser}}>
+        <userContext.Provider value={{userId, userData, setUserId, setUserData}}>
             {children}
         </userContext.Provider>
     );
