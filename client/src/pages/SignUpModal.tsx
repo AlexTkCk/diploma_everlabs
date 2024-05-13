0ba5a892ad2c85bfde95869b39bdc773ddb71555
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-13 13:00:53 +0300

    v-view-authentication/15/Resolve conflicts

--- client/src/pages/SignUpModal.tsx
+++ client/src/pages/SignUpModal.tsx
@@ -1,19 +1,22 @@
-import React, { useContext } from "react";
-import Input from "./Input";
-import Button from "./Button";
+import React, {useContext} from "react";
+import Input from "../components/Input";
+import Button from "../components/Button";
 import { FcGoogle } from "react-icons/fc";
-import { themeContext } from "../context/ThemeContext";
+import {themeContext} from "../context/ThemeContext";
 
 const SignUpModal = () => {
-  const { themeConfig } = useContext(themeContext);
+
+    const {themeConfig} = useContext(themeContext);
 
   return (
-    <div className={"fixed top-0 left-0 w-full h-full grid place-items-center"}>
+      <div className={"relative w-full h-full grid place-items-center"}>
+          <div
+              className={"absolute top-0 left-0 w-full h-full bg-black opacity-80"}
+          ></div>
       <div
-        className={"absolute top-0 left-0 w-full h-full bg-black opacity-80"}
-      ></div>
-      <div
-        className={`w-1/3 -skew-x-12 ${themeConfig.neon} ${themeConfig.info} py-5 px-5`}
+        className={
+          `w-1/3 -skew-x-12 ${themeConfig.neon} ${themeConfig.info} py-5 px-5`
+        }
       >
         <div className={"mx-auto flex flex-col gap-10"}>
           <Input
@@ -34,9 +37,9 @@ const SignUpModal = () => {
           <div className={"flex justify-center items-center gap-5"}>
             <Button
               handler={() => {}}
-              buttonClassName="hover:shadow-buttonHover_md hover:shadow-blue-500 transition-all duration-500 hover:text-white bg-purple-300"
+              buttonClassName="hover:shadow-buttonHover hover:shadow-blue-500 transition-all duration-500 hover:text-white bg-purple-300"
             >
-              Sign up
+                Sign up
             </Button>
             <FcGoogle className={"text-7xl stroke-black stroke-1"} />
           </div>
