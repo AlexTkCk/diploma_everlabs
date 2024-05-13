2d4b531e2e902ffa3852b2885269df8173f36379
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-13 15:47:35 +0300

    v-view-authentication/25/Added mock keep log in data

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, Dispatch, SetStateAction, useState} from 'react';
+import React, {createContext, Dispatch, SetStateAction, useEffect, useState} from 'react';
 import {TUserData} from "../types";
 import {v4 as uuid4} from 'uuid';
 
@@ -20,11 +20,47 @@ export const userContext = createContext<TUserContext>({
     setUserData: () => {}
 });
 
+export const jwtData = [
+    {
+        email: '1',
+        jwt: 'ab1c34a',
+        id: '1',
+        name: 'user1'
+    },
+    {
+        email: '2',
+        jwt: 'a21c3ba',
+        id: '2',
+        name: 'user2'
+    }
+]
+
+
+const getDataByJwt = (jwt: string | null): Promise<TUserData> => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const user = jwtData.find((user) => user.jwt === jwt) as {name: string};
+            if (user)
+                resolve({
+                    name: user.name,
+                })
+
+            resolve(null);
+        }, 1000);
+    })
+}
+
 const UserContext = ({children}: TUserContextProps) => {
     const [userId, setUserId] = useState(uuid4());
     const [userData, setUserData] = useState<TUserData>(null);
 
-    console.log(userId)
+    useEffect(() => {
+        const jwt = localStorage.getItem('jwt');
+        getDataByJwt(jwt).then((data) => {
+            setUserData(data);
+        })
+        console.log(jwtData)
+    }, [userId]);
 
     return (
         <userContext.Provider value={{userId, userData, setUserId, setUserData}}>
