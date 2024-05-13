9af0fa5ef0878081e4ae669ef675204b72eacfc9
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-13 16:09:00 +0300

    v-view-authentication/28/Deleted unnecessary functions

--- client/src/data/mockupAuthFunctions.ts
+++
@@ -1,9 +0,0 @@
-import usersData from './mockupUsers.json';
-import {TUser} from "../types";
-
-export const login = (email: string, password: string): Promise<TUser | {message: string}> => {
-    return new Promise<TUser>((resolve, reject) => setTimeout(() => {
-        const user = usersData.find(user => user.email === email);
-        user?.password === password ? resolve(user) : reject({message: 'Invalid credentials'});
-    }, 1000))
-}
