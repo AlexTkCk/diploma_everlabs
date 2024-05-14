2a551e1479c2a956656767f361100db3167efb8a
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-05-14 14:41:51 +0300

    v-game-room/4/Changed mockup server speed calculations

--- client/src/mockupServer/mockupServer.js
+++ client/src/mockupServer/mockupServer.js
@@ -6,17 +6,17 @@ wss.on('connection', function connection(ws) {
     console.log('A new client connected');
 
     setInterval(() => {
-        ws.send(JSON.stringify({owner: 2, speed: Math.round(Math.random() * 5) - 2}))
-    }, 500)
-    setInterval(() => {
         ws.send(JSON.stringify({owner: 1, speed: -0.5}))
+    }, 100)
+    setInterval(() => {
+        ws.send(JSON.stringify({owner: 2, speed: -0.5}))
     }, 100)
 
     ws.on('message', function incoming(message) {
         const {sym, trueSym} = JSON.parse(message);
 
         if (sym === trueSym) {
-            ws.send(JSON.stringify({owner: 1, speed: 5}));
+            ws.send(JSON.stringify({owner: 1, speed: 2}));
         } else {
             ws.send(JSON.stringify({owner: 1, speed: -2}))
         }
