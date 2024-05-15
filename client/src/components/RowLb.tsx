e6029b16c129e6ed45637310aed3436a480015e0
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-05-15 12:20:44 +0300

    v-7-Leaderboard/8/fixed height of table, set height for row

--- client/src/components/RowLb.tsx
+++ client/src/components/RowLb.tsx
@@ -11,7 +11,7 @@ type TRowLb = {
 
 const RowLb: React.FC<TRowLb> = ({ dataLB }) => {
   return (
-    <div className="flex flex-row justify-around text-center text-xl">
+    <div className="flex flex-row justify-around text-center text-xl h-10">
       <span className="w-1/3">{dataLB.name}</span>
       <span className="w-1/3">{dataLB.speed}</span>
       <span className="w-1/3">{dataLB.time}</span>
