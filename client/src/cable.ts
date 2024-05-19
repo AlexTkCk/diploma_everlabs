189ca8524d4917bad2ccaea74838812ff87b1fb4
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-05-19 22:45:55 +0300

    Deleted cable.ts

--- client/src/cable.ts
+++
@@ -1,8 +0,0 @@
-import { createConsumer } from "@rails/actioncable";
-import {WSSUrl} from "./data/serverUrl";
-
-const actionCableUrl = WSSUrl + '/cable';
-
-const consumer = createConsumer(actionCableUrl);
-
-export default consumer;
