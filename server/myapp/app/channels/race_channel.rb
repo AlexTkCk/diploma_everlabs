eea0c7def3d25723119a2ab426a0df51f49f303d
Vidmenko Oleksij <avidmemko3@gmail.com>
2024-06-02 18:08:41 +0300

    stable-version

--- server/myapp/app/channels/race_channel.rb
+++ server/myapp/app/channels/race_channel.rb
@@ -1,11 +1,22 @@
 class RaceChannel < ApplicationCable::Channel
   def subscribed
     room_id = params[:room_id]
+    user_id = params[:user_id]
     room = Room.find_by(id: room_id)
     stream_from "race_channel_#{room_id}"
+
+    user_entered(room_id, user_id)
+
     puts "Client subscribed to race channel for room #{room_id}"
   end
+  def unsubscribed
+    room_id = params[:room_id]
+    user_id = params[:user_id]
 
+    user_left(room_id, user_id)
+
+    puts "Client unsubscribed from race channel for room #{room_id}"
+  end
   def start_game(data)
     room_id = data["room_id"]
     room = Room.find_by(id: room_id)
@@ -45,6 +56,27 @@ class RaceChannel < ApplicationCable::Channel
       user_id: user_id,
       room_id: room_id,
       speed_change: speed_change
+    }
+
+    ActionCable.server.broadcast("race_channel_#{room_id}", message)
+  end
+
+  private
+  def user_entered(room_id, user_id)
+    message = {
+      user_id: user_id,
+      room_id: room_id,
+      status: "entered"
+    }
+
+    ActionCable.server.broadcast("race_channel_#{room_id}", message)
+  end
+
+  def user_left(room_id, user_id)
+    message = {
+      user_id: user_id,
+      room_id: room_id,
+      status: "left"
     }
 
     ActionCable.server.broadcast("race_channel_#{room_id}", message)
